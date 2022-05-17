ea0d034e7af240040bf4f976980e6f173c99af8f
Talurion <flopebob@gmail.com>
2022-05-17 14:10:07 +1000

    Task1

---
+++ Task1/Task1.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream> 
+
+using namespace std;
+
+bool checkPrimeNumber(int);
+
+
+int main() {
+
+    setlocale(LC_ALL, "");
+    int n;
+    cout << "Введите целочисленное положительное значение: ";
+    cin >> n;
+
+    if (checkPrimeNumber(n))
+        cout << n << " — это простое число.";
+    else
+        cout << n << " — это значение не является простым числом.";
+    return 0;
+
+}
+
+
+
+bool checkPrimeNumber(int n) {
+
+    bool isPrimeNumber = true;
+
+
+
+    // 0 и 1 не являются простыми числами 
+
+    if (n == 0 || n == 1) {
+
+        isPrimeNumber = false;
+
+    }
+
+    else {
+
+        for (int i = 2; i <= n / 2; ++i) {
+
+            if (n % i == 0) {
+
+                isPrimeNumber = false;
+
+                break;
+
+            }
+
+        }
+
+    }
+
+    return isPrimeNumber;
+
+}
